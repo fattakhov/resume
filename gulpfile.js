1f52e923ea04605170a07cfd9fea695a584e9ed1
mansur fat <mansur@homer.local>
2018-08-24 14:30:09 +0300

    first html added

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('html', function(){
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest('build'))
+});
+
+gulp.task('css', function(){
+  return gulp.src('src/scss/*.scss')
+    .pipe(sass())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('build/css'))
+});
+
+gulp.task('js', function(){
+  return gulp.src('src/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(concat('app.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/js'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/js/*.js', ['js']);
+  gulp.watch('src/scss/*.scss', ['css']);
+  gulp.watch('src/*.html', ['html']);
+});
+
+gulp.task('default', [ 'html', 'css', 'js', 'watch' ]);
